18bcc104dcc0c8cc31819e08cceaf5dbc8c8f104
BogdanVereshchak <bohdan.vereshchak.pp.2022@lpnu.ua>
2023-11-23 23:49:40 +0200

    Фікс бага з випадаючою панеллю

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,12 @@
 ﻿const hamMenu = document.querySelector('.ham-menu');
+const navbartoggler = document.querySelector('navbar-toggler');
+let isHamMenuClickable = true;
+
 hamMenu.addEventListener('click', () => {
+    if (!isHamMenuClickable) {
+        return;
+    }
+    isHamMenuClickable = false;
     hamMenu.classList.toggle("active");
-})
+    setTimeout(() => {isHamMenuClickable = true;}, 350);
+});
